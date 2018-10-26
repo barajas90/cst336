909af7271bf3ef5679eed31b28b0205a390ee386
Willliam Barajas <wbarajas@csumb.edu>
2018-10-26 04:10:52 +0000

    adding lab1

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 // then click the URL that is emitted to the Output tab of the console
 
 echo "William Barajas' CST336 Home Page";
-
 ?>
+<br><br>
+<a href="labs/lab1/index.html"> Lab 1 </a>
+
 </body>
 </html>
