2c53b2dc42e6fede9a8422442aaa2ace060927c3
Willliam Barajas <wbarajas@csumb.edu>
2018-10-30 12:36:30 +0000

    adding Lab2

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ echo "William Barajas' CST336 Home Page";
 <br><br>
 <p>LABS</p>
 <a href="labs/lab1/index.html"> Lab 1 </a>
+<a href="labs/lab2/777/index.html"> Lab 2</a>
 <br><br>
 <p>HOMEWORK</p>
 <a href="HW/homework1/index.html"> Homework 1</a>
