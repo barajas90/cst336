c00ed80a7eb753b3bdfda491d42c185083b8947f
Willliam Barajas <wbarajas@csumb.edu>
2018-11-12 15:23:18 +0000

    adding lab3

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -4,7 +4,7 @@
     if(isset($_GET['keyword'])) {
         include 'api/pixabayAPI.php';
         $keyword = $_GET['keyword'];
-        $imageURLs = getImageURLs('$keyword');
+        $imageURLs = getImageURLs($keyword);
         $backgroundImage = $imageURLs[array_rand($imageURLs)];
     }
             
@@ -14,9 +14,7 @@
     <head>
         <title> Lab3: Image Carousel </title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <style>
             @import url("css/styles.css");
             body{
@@ -52,7 +50,7 @@
                         $randomIndex = rand(0, count($imageURLs));
                             
                         }while(!isset($imageURLs[$randomIndex]));
-                        echo'<div class = "item ';
+                        echo'<div class = "carousel-item ';
                         echo ($i == 0)?"active": "";
                         echo '">';
                         echo '<img src="' . $imageURLs[$randomIndex] . '">';
@@ -76,11 +74,11 @@
         ?>
         <br>
         <form>
-            <input type="text" name="keyword" placeholder="Keyword" value= "<?=$_GET ['keyword']?>"/>
+            <input type="text" name="keyword" placeholder="keyword" value= "<?=$_GET ['keyword']?>"/>
             <input type="radio" id="lhorizontal" name="layout" value"horizontal">
-            <label for = "Horizontal"><label for="lhorizontal"> Horizontal </label>
+            <label for = "Horizontal"></label><label for="lhorizontal"> Horizontal </label>
             <input type="radio" id="lvertical" name="layout" value="vertical">
-            <label for="vertical"></label><label for="lvertical">Vertical</label>
+            <label for="Vertical"></label><label for="lvertical">Vertical</label>
             <select name= "category">
                 <option value="">Select One</option>
                 <option value="ocean">Sea</option>
@@ -88,7 +86,7 @@
                 <option>Mountain</option>
                 <option>Snow</option>
             </select>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Search" />
         </form>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
